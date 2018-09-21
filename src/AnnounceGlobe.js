061d2bf6fe63f04ed9c4bcfcfa8cbede5156cd6d
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2018-09-21 10:03:29 -0400

    Notification showed on Book shelf changed

---
+++ src/AnnounceGlobe.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+class AnnounceGlobe extends Component {
+    render () {
+        return (
+            <div className="announce-globe">{this.props.message}</div>
+        )
+    }
+}
+
+export default AnnounceGlobe
--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Route } from 'react-router-dom'
 import './App.css'
 import * as BooksAPI from './BooksAPI'
 import BookList from './BookList'
+import AnnounceGlobe from './AnnounceGlobe'
+
 import SearchBook from './SearchBook'
 
 
 class BooksApp extends React.Component {
       state = {
           books: [],
+          announceGlobeShow: false
       }
 
       updateBooks = () => {
@@ -17,22 +20,33 @@ class BooksApp extends React.Component {
           })
       }
 
-      componentDidMount() {
+      componentDidMount = () => {
           this.updateBooks()
       }
 
-      changeBookShelf = (book, newShelft) => {
-          BooksAPI.update(book, newShelft).then(() => (
+      changeBookShelf = (book, newShelf) => {
+          BooksAPI.update(book, newShelf).then(() => (
               this.updateBooks()
+
+          )).then(() => (
+              this.showAnnounceGlobe()
           ))
 
+      }
+
+      showAnnounceGlobe = () => {
+          this.setState({announceGlobeShow: true})
+          setTimeout(() => (
+              this.setState({announceGlobeShow: false})
+          ), 4000)
       }
 
       render() {
           return (
               <div className="app">
+                  {this.state.announceGlobeShow && (<AnnounceGlobe message="Book Moved Succesfully!"/>)}
                   <Route exact path="/" render={() => (
-                          <BookList booksObject={this.state.books} changeBookShelfFunction={this.changeBookShelf}/>
+                          <BookList booksObject={this.state.books} changeBookShelfFunction={this.changeBookShelf} />
                   )}/>
                   <Route exact path="/search" render={() => (
                           <SearchBook actualBooksObject={this.state.books} changeBookShelfFunction={this.changeBookShelf}/>
